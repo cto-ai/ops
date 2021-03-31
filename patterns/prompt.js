915772a6074f59f0c50c735fc0c5ff69a5d9cffc
David Mark Clements <david.mark.clements@gmail.com>
2021-04-01 01:04:53 +0200

    mv confirm format into prompt pattern

--- patterns/prompt.js
+++ patterns/prompt.js
@@ -2,7 +2,8 @@ import enquirer from 'enquirer'
 import { render } from '@cto.ai/tux'
 
 export async function prompt (info) {
-  const { ns, questions, ...opts } = info
+  const { ns, ...opts } = info
+  if (info.type === 'confirm') opts.format = (sure) => sure ? 'Yes' : 'No'
   const response = await enquirer.prompt({
     ...opts,
     styles: {
